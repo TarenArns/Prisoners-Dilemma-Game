9282f29ca8f636d40d4e5c3ea571ddb9e9521a43
Taren Arns <tarenarns@outlook.com>
2026-02-20 16:07:25 -0600

    login button sends requests

--- client/Prisoners-Dillema-Frontend/src/components/loginForm/LoginForm.tsx
+++ client/Prisoners-Dillema-Frontend/src/components/loginForm/LoginForm.tsx
@@ -1,9 +1,21 @@
+import axios from 'axios';
 import './loginForm.css'
 
+
+
 function LoginForm() {
+
+    async function handleSubmit(formData: FormData) {
+        const username = formData.get('username')
+        let url = 'http://localhost:6769/api/login';
+
+        const response = await axios.post(url, {user: username});
+        console.log(response.data)
+    }
+
     return (
-        <form className="login-form">
-            <input type="text" placeholder="Username" className="login-input" />
+        <form className="login-form" action={handleSubmit}>
+            <input type="text" placeholder="Username" name='username' className="login-input" />
             <button type="submit" className="login-button">Login</button>
         </form>
     )
