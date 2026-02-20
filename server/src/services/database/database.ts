a1c0f5d62a565f2e37e2c02c9c5ed38ccb54d716
Daniel Markuza <daniel.markuza@gmail.com>
2026-02-20 15:12:07 -0600

    server revamp

---
+++ server/src/services/database/database.ts
@@ -0,0 +1,13 @@
+
+
+function addRecord(table: any, key: string, fields: any) {
+    
+}
+
+function deleteRecord(table: any, key: string, fields: any) {
+
+}
+
+function fetchRecord(tabe: any, key: string) {
+    return null
+}
