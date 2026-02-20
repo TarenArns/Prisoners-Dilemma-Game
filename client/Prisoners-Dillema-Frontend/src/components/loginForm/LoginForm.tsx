9b281b9639fe01172cb803e5e82b3ee81eaf373d
Taren Arns <tarenarns@outlook.com>
2026-02-20 17:26:58 -0600

    fix tailwind and minor styling

--- client/Prisoners-Dillema-Frontend/src/components/loginForm/LoginForm.tsx
+++ client/Prisoners-Dillema-Frontend/src/components/loginForm/LoginForm.tsx
@@ -1,8 +1,8 @@
 function LoginForm() {
     return (
         <form className="flex flex-col gap-4 max-w-sm">
-            <input type="text" placeholder="Username" className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
-            <button type="submit" className="px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors">Login</button>
+            <input type="text" placeholder="Username" className="px-4 py-2 border border-primary rounded-lg focus:outline-none focus:ring-2 focus:ring-secondary" />
+            <button type="submit" className="px-6 py-2 bg-secondary text-white rounded-lg hover:bg-primary transition-colors">Login</button>
         </form>
     )
 }
--- client/Prisoners-Dillema-Frontend/src/pages/lobbySelect/LobbySelectPage.tsx
+++ client/Prisoners-Dillema-Frontend/src/pages/lobbySelect/LobbySelectPage.tsx
@@ -2,9 +2,9 @@ import LobbySelectButtons from '../../components/lobbySelectButtons/LobbySelectB
 
 function LobbySelect() {
   return (
-    <div className='flex flex-col items-center gap-4 p-8'>
-      <h2>Please select the type of lobby you want to join</h2>
-      <text> you can either join a public lobby or create a private lobby and invite your friends</text>
+    <div className="flex flex-col items-center justify-center bg-tertiary h-screen w-screen">
+      <h2 className='text-3xl font-bold mb-4 text-gray-800'>Please select the type of lobby you want to join</h2>
+      <text className='text-gray-600 mb-6 text-center'> you can either join a public lobby or create a private lobby and invite your friends</text>
       <LobbySelectButtons />
     </div>
   )
