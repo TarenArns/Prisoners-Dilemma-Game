a5f8bd0d50eaf03113096dd4bce94120fe9b9690
Taren Arns <tarenarns@outlook.com>
2026-02-20 17:26:58 -0600

    fix tailwind and minor styling

--- client/Prisoners-Dillema-Frontend/src/components/lobbySelectButtons/LobbySelectButtons.tsx
+++ client/Prisoners-Dillema-Frontend/src/components/lobbySelectButtons/LobbySelectButtons.tsx
@@ -1,9 +1,9 @@
 function LobbySelectButtons() {
     return (
-        <div className='lobbySelectContainer'>
-            <button className='lobbySelectButton'>Join Public Lobby</button>
-            <button className='lobbySelectButton'>Join Private Lobby</button>
-            <button className='lobbySelectButton'>Create Private Lobby</button>
+        <div className='lobbySelectContainer flex flex-col gap-4'>
+            <button className='lobbySelectButton px-6 py-2 bg-secondary text-white rounded-lg hover:bg-primary transition-colors'>Join Public Lobby</button>
+            <button className='lobbySelectButton px-6 py-2 bg-secondary text-white rounded-lg hover:bg-primary transition-colors'>Join Private Lobby</button>
+            <button className='lobbySelectButton px-6 py-2 bg-secondary text-white rounded-lg hover:bg-primary transition-colors'>Create Private Lobby</button>
         </div>
     );
 }
