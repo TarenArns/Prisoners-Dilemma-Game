97f60e5dea56e9402f1fae04a44d52fdc3aa8e9f
Taren Arns <tarenarns@outlook.com>
2026-02-20 16:07:25 -0600

    Fixing merge conflicts

--- client/Prisoners-Dillema-Frontend/src/components/loginForm/LoginForm.tsx
+++ client/Prisoners-Dillema-Frontend/src/components/loginForm/LoginForm.tsx
@@ -1,4 +1,13 @@
 function LoginForm() {
+
+    async function handleSubmit(formData: FormData) {
+        const username = formData.get('username')
+        let url = 'http://localhost:6769/api/login';
+
+        const response = await axios.post(url, {user: username});
+        console.log(response.data)
+    }
+
     return (
         <form className="flex flex-col gap-4 max-w-sm">
             <input type="text" placeholder="Username" className="px-4 py-2 border border-primary rounded-lg focus:outline-none focus:ring-2 focus:ring-secondary" />
