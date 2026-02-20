484cb54760812099d68b974e2a67312350248f01
Taren Arns <tarenarns@outlook.com>
2026-02-20 15:31:51 -0600

    use tailwind over css files

--- client/Prisoners-Dillema-Frontend/src/pages/lobbySelect/LobbySelectPage.tsx
+++ client/Prisoners-Dillema-Frontend/src/pages/lobbySelect/LobbySelectPage.tsx
@@ -1,9 +1,8 @@
 import LobbySelectButtons from '../../components/lobbySelectButtons/LobbySelectButtons'
-import './LobbySelectPage.css'
 
 function LobbySelect() {
   return (
-    <div className='lobbySelectPage'>
+    <div className='flex flex-col items-center gap-4 p-8'>
       <h2>Please select the type of lobby you want to join</h2>
       <text> you can either join a public lobby or create a private lobby and invite your friends</text>
       <LobbySelectButtons />
