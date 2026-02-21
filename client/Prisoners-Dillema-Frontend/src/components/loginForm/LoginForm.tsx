418ca48bd531a4ec6747a3e869682d055330a703
Taren Arns <tarenarns@outlook.com>
2026-02-20 18:59:15 -0600

    persistant connection to server

--- client/Prisoners-Dillema-Frontend/src/components/loginForm/LoginForm.tsx
+++ client/Prisoners-Dillema-Frontend/src/components/loginForm/LoginForm.tsx
@@ -1,16 +1,17 @@
-import { postRequest } from '../../utils/apiRequests';
+import { useSocket } from '../../hooks/useSocket';
 
 function LoginForm() {
+    const socket = useSocket();
 
     function handleSubmit(formData: any) {
-        const username = formData.get('username')
-        postRequest('/login', { user: username })
+        const username = formData.get('username');
+        socket.emit('/login', { user: username });
     }
 
     return (
-        <form className="flex flex-col gap-4 max-w-sm">
-            <input type="text" placeholder="Username" className="px-4 py-2 border border-primary rounded-lg focus:outline-none focus:ring-2 focus:ring-secondary" />
-            <button type="submit" className="px-6 py-2 bg-secondary text-white rounded-lg hover:bg-primary transition-colors">Login</button>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 max-w-sm">
+            <input name="username" type="text" placeholder="Username" className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
+            <button type="submit" className="px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors">Login</button>
         </form>
     )
 }
