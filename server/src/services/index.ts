361e814de666321338fdb5f79ef464fb2c015c21
leonk <katsnelson.leon@gmail.com>
2026-02-21 11:19:23 -0600

    restructured game logic away from oo, adjusted interface for api. API will only interact with Game object

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -23,29 +23,55 @@ const MATCH_ROUNDS = 10
 // TODO: scores could be configurable?
 const SCORING = { // SCORING[choice1][choice2] returns [score1, score2]
     // TODO: 2(cc) > (cd) + (dc) is a condition?
-    0: { 0: [1, 1], 1: [3, 0] },
-    1: { 0: [0, 3], 1: [2, 2] }
+    false: { false: [1, 1], true: [3, 0] },
+    true: { false: [0, 3], true: [2, 2] }
 }
 
-const Choice = { def: 0, coop: 1 } as const
+const Choice = { def: false, coop: true } as const
 type Choice = (typeof Choice)[keyof typeof Choice]
 
-abstract class Prisoner {
+const PrisType = {
+    // player: "Player",
+    random: "Random",
+    cooperator: "Cooperator",
+    defecator: "Defecator",
+    tit4tat: "Tit4Tat",
+} as const;
+type PrisType = (typeof PrisType)[keyof typeof PrisType]
+
+const choose = (type: PrisType, history: Choice[]) => {
+    switch (type) {
+        case PrisType.random :
+            return Math.random() < 0.5
+        case PrisType.cooperator : 
+            return Choice.coop
+        case PrisType.defecator : 
+            return Choice.def
+        case PrisType.tit4tat :
+            if (history[history.length-1] == Choice.def) {
+                return Choice.def
+            } else return Choice.coop
+    }
+}
+
+class Prisoner {
     private _user: string;
     private _score: number = 0
     private _history: Choice[] = []
+    private _type: PrisType
 
-    protected abstract choose(oppHistory: Choice[]): Choice
+    public constructor(user: string, type: PrisType) {
+        this._user = user
+        this._type = type
+    }
 
     public async makeChoice(oppHistory: Choice[]) {
         let c = await new Promise<Choice>((resolve) => {
-            resolve(this.choose(oppHistory))
+            resolve(choose(this._type, oppHistory))
         })
         this._history.push(c)
         return c
     }
-
-    public constructor(user: string) { this._user = user }
 
     public addScore(score: number): void {
         this._score += score
@@ -55,35 +81,6 @@ abstract class Prisoner {
     public get score(): number { return this._score }
     public get history(): Choice[] {return this._history}
 }
-
-class Random extends Prisoner {
-    protected choose(): Choice { // TODO: implement choosing
-        return Math.random() < 0.5 ? Choice.def : Choice.coop;
-    }
-}
-
-/*
-class Player extends Prisoner {
-    protected choose(history: Choice[]): Choice {
-        return Choice.def
-    }
-}
-
-class Cooperator extends Prisoner {
-    protected choose(history: Choice[]): Choice { return Choice.coop }
-}
-
-class Defecator extends Prisoner {
-    protected choose(history: Choice[]): Choice { return Choice.def }
-}
-
-class Tit4Tat extends Prisoner {
-    protected choose(history: Choice[]): Choice {
-        if (history[history.length-1] == Choice.def) { return Choice.def }
-        else { return Choice.coop }
-    }
-}
-*/
 
 class Match {
     private p1: Prisoner
@@ -100,14 +97,15 @@ class Match {
 
     public async run() {
         for (let i=0; i<this.length; i++) {
-            // TODO: should be async or something?
 
+            // request choice from both players
             let choices = await Promise.all([
                 this.p1.makeChoice(this.p2.history),
                 this.p2.makeChoice(this.p1.history)
             ])
 
-            this.history.push([choices[0], choices[1]]) // TODO how to handle history?
+            // TODO how to handle history?
+            this.history.push([choices[0], choices[1]])
             let scores = SCORING[`${choices[0]}`][`${choices[1]}`]
             this.p1.addScore(scores[0])
             this.p2.addScore(scores[1])
@@ -119,29 +117,31 @@ class Match {
 }
 
 class Game {
-    private prisoners: Prisoner[]
-
-    public constructor(prisoners: Prisoner[]) {
-        this.prisoners = prisoners;
-    }
+    private _prisoners: Prisoner[] = []
 
     public async run() {
         // Pair up all players
         let matchThreads: Promise<void>[] = []
-        for (let i in this.prisoners) {
-            let p1 = this.prisoners[i];
-            for (let j in this.prisoners.slice(+i+1)) {
-                let p2 = this.prisoners[j];
+        for (let i in this._prisoners) {
+            let p1 = this._prisoners[i];
+            for (let j in this._prisoners.slice(+i+1)) {
+                let p2 = this._prisoners[j];
                 matchThreads.push(new Match(p1, p2, MATCH_ROUNDS).run())
             }
         }
-
         await Promise.all(matchThreads)
 
-        for (let p of this.prisoners) {
+        for (let p of this._prisoners) {
             console.log(`${p.user}: ${p.score}`)
         }
+    }
 
+    public get prisoners() {return this._prisoners}
+    public addPrisoner(user: string, type: PrisType) {
+        this._prisoners.push(new Prisoner(user, type))
+    }
+    public removePrisoner(user: string) {
+        delete this._prisoners[this._prisoners.findIndex((p) => p.user == user)]
     }
 }
 
@@ -150,9 +150,12 @@ class Game {
  **************************************************/
 
 // Initialize game with players (simulated)
-await new Game(
-    [new Random("hi"), new Random("me")]
-    // [new Defecator("Defecator1"), new Tit4Tat("Tit4Tat1"),
-    // new Defecator("Defecator2"), new Cooperator("Cooperator1"),
-    // new Random("Random1")]
-).run()
+
+let g = new Game()
+
+g.addPrisoner("Leon", "Random")
+g.addPrisoner("Daniel", "Random")
+g.addPrisoner("Taren", "Random")
+g.addPrisoner("Taren", "Random")
+
+await g.run()
