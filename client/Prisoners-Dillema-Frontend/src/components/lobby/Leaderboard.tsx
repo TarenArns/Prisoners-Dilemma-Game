32012e1837c93a864cc54f68d34f10a23641634a
Taren Arns <tarenarns@outlook.com>
2026-02-21 00:53:57 -0600

    leaderboard as lobby page players list

---
+++ client/Prisoners-Dillema-Frontend/src/components/lobby/Leaderboard.tsx
@@ -0,0 +1,29 @@
+interface LeaderboardProps {
+    players: any[]
+}
+
+function Leaderboard({ players }: LeaderboardProps) {
+    return (<div className='bg-secondary rounded-lg p-6 flex flex-col h-full'>
+        <h3 className='text-xl font-bold mb-4 text-black'>Players in the lobby:</h3>
+        <table className='border-collapse'>
+            <thead>
+                <tr>
+                    <th className='border border-primary px-4 py-2'>Player</th>
+                    <th className='border border-primary px-4 py-2'>Points</th>
+                    <th className='border border-primary px-4 py-2'>Blame/Don't</th>
+                </tr>
+            </thead>
+            <tbody>
+                {players.map((player) => (
+                    <tr key={player.name}>
+                        <td className='border border-primary px-4 py-2'>{player.name}</td>
+                        <td className='border border-primary px-4 py-2 text-center'>{player.points}</td>
+                        <td className='border border-primary px-4 py-2 text-center'>{player.blameRatio}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+    )
+}
+export default Leaderboard
