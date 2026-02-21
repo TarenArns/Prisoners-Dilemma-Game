c886e415cd02f994443d415e2e866ab6e33902b4
Daniel Markuza <daniel.markuza@gmail.com>
2026-02-20 19:53:33 -0600

    LOGIN WORKING

--- client/Prisoners-Dillema-Frontend/src/components/loginForm/LoginForm.tsx
+++ client/Prisoners-Dillema-Frontend/src/components/loginForm/LoginForm.tsx
@@ -1,21 +1,20 @@
-import { useNavigate } from 'react-router-dom';
-import { useSocket } from '../../hooks/useSocket';
+import { postRequest } from '../../utils/apiRequests';
 
 function LoginForm() {
-    const socket = useSocket();
-    const navigate = useNavigate();
 
-    function handleSubmit(formData: any) {
-        const username = formData.get('username');
-        socket.emit('login', { user: username });
-        socket.on("request_response", () => {
-            navigate('/lobbyselect');
-        })
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const form = e.currentTarget;
+        const formData = new FormData(form);
+        const username = formData.get("username");
+
+        const response = await postRequest("/login", { user: username });
+        console.log(response.data);
     }
 
     return (
         <form onSubmit={handleSubmit} className="flex flex-col gap-4 max-w-sm">
-            <input name="username" type="text" placeholder="Username" className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
+            <input type="text" placeholder="Username" name="username" className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
             <button type="submit" className="px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors">Login</button>
         </form>
     )
