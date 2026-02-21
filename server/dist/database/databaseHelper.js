16aaa2e29ab3d891df7eb0ff134fefe7e04e96cb
leonk <katsnelson.leon@gmail.com>
2026-02-20 19:41:35 -0600

    Tried to resolve conflicts by adding garbo to gitignores

--- server/dist/database/databaseHelper.js
+++
@@ -1,9 +0,0 @@
-export function addRecord(table, fields) {
-}
-export function updateRecord(table, key, fields) {
-}
-export function deleteRecord(table, key, fields) {
-}
-export function fetchRecord(tabe, key) {
-    return null;
-}
