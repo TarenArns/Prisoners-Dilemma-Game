2793fdb683afccbc94be10f19883ef5aa6e6efad
Taren Arns <tarenarns@outlook.com>
2026-02-21 10:36:23 -0600

    only show players buttons

--- client/Prisoners-Dillema-Frontend/src/components/game/PlayerHalfScreen.tsx
+++ client/Prisoners-Dillema-Frontend/src/components/game/PlayerHalfScreen.tsx
@@ -2,14 +2,20 @@ interface PlayerPanelProps {
     playerName: string;
     bgColor: string;
     avatarBgColor: string;
+    isClient: boolean;
 }
 
-function PlayerHalfScreen({ playerName, bgColor, avatarBgColor }: PlayerPanelProps) {
+function PlayerHalfScreen({ playerName, bgColor, avatarBgColor, isClient }: PlayerPanelProps) {
     return (
         <div className={`w-1/2 flex flex-col items-center justify-center ${bgColor}`}>
             <div className={`w-32 h-32 rounded-full ${avatarBgColor} mb-6 text-center`}>{playerName}</div>
-            <button className='mb-2 px-6 py-2 bg-tertiary rounded'>Defect</button>
-            <button className='px-6 py-2 bg-tertiary rounded'>Cooperate</button>
+            {isClient &&
+                <div className="flex flex-col items-center justify-center">
+                    <button className='mb-2 px-6 py-2 bg-tertiary rounded hover:bg-red-500 transition-colors'>Defect</button>
+                    <button className='px-6 py-2 bg-tertiary rounded hover:bg-green-500 transition-colors'>Cooperate</button>
+                </div>
+            }
+
         </div>
     )
 }
