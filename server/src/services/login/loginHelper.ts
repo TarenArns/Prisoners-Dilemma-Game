8fe4778e65168e1da6e3bf0ccad54d3516fb1483
Daniel Markuza <daniel.markuza@gmail.com>
2026-02-20 18:18:21 -0600

    fix login

--- server/src/services/login/loginHelper.ts
+++
@@ -1,8 +0,0 @@
-import { fetchRecord, addRecord } from "../database/databaseHelper.js"
-import { PLAYER_TABLE } from "../database/constants.js"
-
-export function login(user:any) {
-    if(user && !fetchRecord(PLAYER_TABLE, user)) {
-        addRecord(PLAYER_TABLE, {user: user})
-    }
-}
